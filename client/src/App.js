0d4c4683f7b2c9eb9ac95d9269623e7e4db908cc
codeComet <unknownmind10@gmail.com>
2021-12-26 03:18:27 +0600

    all markers are showing countrywise, needs to add description and rating

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,48 @@
 import React, { useState, useEffect } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import axios from "axios";
 
 function App() {
-  const [lat, setLat] = useState(48.8584);
-  const [lng, setLng] = useState(2.2945);
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [pins, setPins] = useState([]);
+
+  // const API = axios.create({
+  //   baseURL: "http://localhost:5000",
+  // });
+
   const [viewport, setViewport] = useState({
-    width: "100vw",
-    height: "100vh",
+    width: "90vw",
+    height: "90vh",
     latitude: lat,
     longitude: lng,
     zoom: 4,
   });
 
   useEffect(() => {
+    let isMounted = true;
+    const getPins = async () => {
+      try {
+        const { data } = await axios.get("/pins");
+        setPins(data);
+        console.log(pins);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPins();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       async (position) => {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
         await setLat(position.coords.latitude);
         await setLng(position.coords.longitude);
       },
@@ -31,9 +58,29 @@ function App() {
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
         mapStyle={process.env.REACT_APP_MAP_STYLE}
       >
-        <Marker latitude={lat} longitude={lng} offsetLeft={-20} offsetTop={-10}>
-          <FaMapMarkerAlt style={{ fontSize: "2.5rem", color: "#ff0984" }} />
-        </Marker>
+        {pins.map((pin, idx) => (
+          <div key={idx}>
+            <Marker
+              latitude={pin.lat}
+              longitude={pin.long}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <FaMapMarkerAlt
+                style={{ fontSize: "2.5rem", color: "#ff0984" }}
+              />
+            </Marker>
+            {/* <Popup
+              latitude={pin.lat}
+              longitude={pin.long}
+              closeButton={true}
+              closeOnClick={false}
+              anchor="left"
+            >
+              <div>You are here</div>
+            </Popup> */}
+          </div>
+        ))}
       </ReactMapGL>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import pinRoutes from "./routes/pins.js";
 import userRoutes from "./routes/users.js";
+import cors from "cors";
 import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 dotenv.config();
+app.use(cors());
 app.use("/pins", pinRoutes);
 app.use("/user", userRoutes);
 
